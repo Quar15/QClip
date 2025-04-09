5cf4d9cabf64942e1d131e3e6029b6433f52d4a6
mrugacz <mrugalski.kuba@gmail.com>
2025-04-09 22:26:17 +0200

    fix(thumbnails): scale down image on creation

--- app/utils/files.py
+++ app/utils/files.py
@@ -9,6 +9,7 @@ def is_allowed_mime_type(file, allowed_mime_types: list) -> bool:
 
 
 def generate_thumbnail(file_path: str, out_path: str):
-    cmd = ["ffmpeg", "-v", "quiet", "-y", "-i", file_path, "-ss", "0", "-vframes", "1", out_path]
+    # get 1st frame, scale it down (or up) to 720px wide, save as jpg
+    cmd = ["ffmpeg", "-v", "quiet", "-y", "-i", file_path, "-ss", "0", "-vframes", "1", "-vf", "scale=720:-1", out_path]
 
     subprocess.call(cmd)
